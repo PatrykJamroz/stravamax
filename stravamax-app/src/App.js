a082457138b3436811f36af7e2f2cf8f4cdeb5cc
Patryk J <jamroz.patryk@gmail.com>
2020-09-26 10:22:08 +0200

    unifying code with async/await instead of promises, code indentation

--- stravamax-app/src/App.js
+++ stravamax-app/src/App.js
@@ -10,67 +10,51 @@ function App() {
 
   const [activities, setActivities] = useState([])
   
-  function reAuthorize(){
+  const reAuthorize = () => {
 
-    let clientId 
-    let clientSecret 
-    let refreshToken 
     const auth_link = "https://www.strava.com/oauth/token"
         
     return fetch(auth_link,{
-            method: 'post',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-
-            },
-
-            body: JSON.stringify({
-
-                client_id: crede.clientId,
-                client_secret: crede.clientSecret,
-                refresh_token: crede.refreshToken,
-                grant_type: 'refresh_token'
-            })
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+        },
+      body: JSON.stringify({
+        client_id: crede.clientId,
+        client_secret: crede.clientSecret,
+        refresh_token: crede.refreshToken,
+        grant_type: 'refresh_token'
         })
-    
+    })
+  }
+
+  const getActivities = async () => {
+    const response = await reAuthorize()
+    const data = await response.json()
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?page=1&per_page=10&access_token=${data.access_token}`
+    const activitiesPromise = await fetch(activities_link)
+    const activitiesData = await activitiesPromise.json()
+    setActivities(activitiesData)
   }
 
   useEffect(() => {
-
-      async function getActivities() {
-        const response = await reAuthorize()
-        const data = await response.json()
-        const activities_link = `https://www.strava.com/api/v3/athlete/activities?page=1&per_page=10&access_token=${data.access_token}`
-        
-        
-        fetch(activities_link)
-          .then(response => response.json())
-          .then(data => {
-            setActivities(data)
-          })
-      }
-      getActivities()
-    }, []
+    getActivities()
+  },[]
   )
   
-  
-    
-    return (
-      <div className="App">
-        
-        
-        {activities.map(activity => <div key={activity.id}> 
-          <ActivityName activityName={activity.name}/>
-          <MaxSpeed maxSpeed = {activity.max_speed}/>
-          <MaxHr maxHr = {activity.max_heartrate}/>
-          <Distance distance = {activity.distance}/>
-          <ElevationGain elevationGain = {activity.total_elevation_gain}/>
-          <hr />
-        </div>)}
-                
-      </div>
-    )
+  return (
+    <div className="App">             
+      {activities.map(activity => <div key={activity.id}> 
+        <ActivityName activityName={activity.name}/>
+        <MaxSpeed maxSpeed = {activity.max_speed}/>
+        <MaxHr maxHr = {activity.max_heartrate}/>
+        <Distance distance = {activity.distance}/>
+        <ElevationGain elevationGain = {activity.total_elevation_gain}/>
+        <hr />
+      </div>)}       
+    </div>
+  )
   
 }
 
