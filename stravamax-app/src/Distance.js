2ebe091355f83033f65444e4e43131fcdd6895bf
PJ <jamroz.patryk@gmail.com>
2020-10-11 20:11:43 +0200

    added units conversion btn for speed and distance

--- stravamax-app/src/Distance.js
+++ stravamax-app/src/Distance.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Distance(props) {
-  return <h3>Distance: {(props.distance / 1000).toFixed(1)} km</h3>;
+  const [unit, setUnit] = useState("metric");
+
+  function changeUnit() {
+    setUnit(unit === "metric" ? "imperial" : "metric");
+  }
+
+  return (
+    <div>
+      <h3>
+        Distance:{" "}
+        {unit === "metric"
+          ? `${(props.distance / 1000).toFixed(1)} km`
+          : `${(props.distance / 1609.344).toFixed(1)} mi`}
+      </h3>
+      <button onClick={changeUnit}>
+        Change to {unit === "metric" ? "mi" : "km"}
+      </button>
+    </div>
+  );
 }
 
 export default Distance;
--- stravamax-app/src/MaxSpeed.js
+++ stravamax-app/src/MaxSpeed.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function MaxSpeed(props) {
-  return <h3>Max speed: {(props.maxSpeed * 3.599).toFixed(1)} km/h</h3>;
+  const [unit, setUnit] = useState("metric");
+
+  function changeUnit() {
+    setUnit(unit === "imperial" ? "metric" : "imperial");
+  }
+
+  //The maximum speed of this lat, in meters per second
+
+  return (
+    <div>
+      <h3>
+        Max speed:{" "}
+        {unit === "imperial"
+          ? `${(props.maxSpeed * 2.24).toFixed(1)} mph`
+          : `${(props.maxSpeed * 3.599).toFixed(1)} kmh`}
+      </h3>
+      <button onClick={changeUnit}>
+        Change to {unit === "metric" ? "mph" : "kmh"}
+      </button>
+    </div>
+  );
 }
 
 export default MaxSpeed;
