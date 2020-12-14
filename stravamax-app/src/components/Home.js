91cf014a82b32d723ef724917567ed83ae8dc2fb
Patryk J <jamroz.patryk@gmail.com>
2020-12-14 21:59:31 +0100

    added links to switch between home and activities feed

--- stravamax-app/src/components/Home.js
+++ stravamax-app/src/components/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <h1>Welcome to my app!</h1>;
+  return (
+    <div>
+      <h1>Welcome to my app!</h1>
+      <Link to="/activities">Click here to check activities</Link>
+    </div>
+  );
 }
--- stravamax-app/src/components/NavBar.js
+++ stravamax-app/src/components/NavBar.js
@@ -2,12 +2,14 @@ import React from "react";
 import Logo from "../Logo";
 import Filters from "./Filters";
 import "../styles/NavBar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className="NavBar">
       <Logo />
       <Filters />
+      <Link to="/">Home</Link>
     </div>
   );
 }
