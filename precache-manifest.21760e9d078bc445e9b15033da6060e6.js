db8347366322c87116f714667cb9eaecd171c75d
Patryk J <jamroz.patryk@gmail.com>
2021-02-07 17:04:20 +0100

    Updates

---
+++ precache-manifest.21760e9d078bc445e9b15033da6060e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553c847f56e298f4e14100fd05b3ef21",
+    "url": "/stravamax/stravamax-app/index.html"
+  },
+  {
+    "revision": "da8b6a8c45d57da85d3c",
+    "url": "/stravamax/stravamax-app/static/js/2.4129780e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stravamax/stravamax-app/static/js/2.4129780e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b87431248fdef231b7",
+    "url": "/stravamax/stravamax-app/static/js/main.829723ff.chunk.js"
+  },
+  {
+    "revision": "d6f61e9e0e28653ce70c",
+    "url": "/stravamax/stravamax-app/static/js/runtime-main.a5ce71b9.js"
+  }
+]);
